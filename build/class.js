717b645cb9372499ddf159eb493af9a40123b27c
Constantin Suiu <constantin.suiu@me.com>
2021-10-17 11:24:44 +0200

    Base project structure

---
+++ build/class.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _default = new class Test {
+  test() {
+    console.log('Yes, it works');
+  }
+
+}();
+
+exports.default = _default;
